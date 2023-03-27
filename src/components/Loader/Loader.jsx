544e7ab8dd01a913e929f2684ce93b0a7f8332b1
Jeancarlo Morales <jcode2006@gmail.com>
2023-03-26 19:47:46 -0500

    feat(components): add Loader component

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,11 @@
+const Loader = () => {
+  return (
+    <div className='absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center'>
+      <div className='animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-red-600'>
+        {' '}
+      </div>
+    </div>
+  )
+}
+
+export default Loader
