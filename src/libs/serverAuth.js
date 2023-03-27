adcf4af4988e9941025c3e21ee119957bfb64887
Jeancarlo Morales <jcode2006@gmail.com>
2023-03-26 19:39:20 -0500

    feat(libs): add server-side authentication utility function

---
+++ src/libs/serverAuth.js
@@ -0,0 +1,24 @@
+import { getSession } from 'next-auth/react'
+import prisma from './prismadb'
+
+const serverAuth = async (req) => {
+  const session = await getSession({ req })
+
+  if (!session?.user?.email) {
+    throw new Error('Not authenticated')
+  }
+
+  const currentUser = await prisma.user.findUnique({
+    where: {
+      email: session.user.email
+    }
+  })
+
+  if (!currentUser) {
+    throw new Error('User not found')
+  }
+
+  return { currentUser }
+}
+
+export default serverAuth
