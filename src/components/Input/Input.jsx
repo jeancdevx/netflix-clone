330498e8058eaab9552792c5de86e073e6dd8911
Jeancarlo Morales <jcode2006@gmail.com>
2023-03-26 19:47:36 -0500

    feat(components): add Input component

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,23 @@
+const Input = ({ id, onChange, value, label, type }) => {
+  return (
+    <div className='relative'>
+      <input
+        onChange={onChange}
+        value={value}
+        type={type}
+        id={id}
+        className='block rounded-md px-6 pt-6 pb-1 w-full text-md font-semibold text-white bg-neutral-700 appearance-none focus:outline-none focus:ring-0 peer invalid:border-b-1'
+        placeholder=' '
+      />
+      <label
+        htmlFor={id}
+        className=' absolute text-m text-zinc-400 duration-150 transform  -translate-y-3 scale-75 top-4 z-10 origin-[0] left-6 font-semibold peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-3
+      '
+      >
+        {label}
+      </label>
+    </div>
+  )
+}
+
+export default Input
